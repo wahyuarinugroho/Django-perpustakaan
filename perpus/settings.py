a970579ab75052d4171fcb79af8c34a7aba3c940
wahyuarinugroho <wahyuari.solo@gmail.com>
2021-10-27 06:55:31 +0700

    setup database mysql

--- perpus/settings.py
+++ perpus/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'perpus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'perpus',
+        'USER': 'root',
+        'PASSWORD': 'bi5millah',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
