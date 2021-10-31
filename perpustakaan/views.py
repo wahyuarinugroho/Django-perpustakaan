c22fc27dd1da722cb02788b650ee98fd9230abba
wahyuarinugroho <wahyuari.solo@gmail.com>
2021-10-31 23:24:27 +0700

    form tahap 1

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from perpustakaan.models import Buku
+from perpustakaan.forms import FormBuku
 
-def buku(requst):
+def buku(request):
     books = Buku.objects.all()
     konteks = {
         'books': books,
     }
-    return render(requst, 'buku.html', konteks)
+    return render(request, 'buku.html', konteks)
 
-def penerbit(requst):
-    return render(requst, 'penerbit.html')
+def penerbit(request):
+    return render(request, 'penerbit.html')
+
+def tambah_buku(request):
+    form = FormBuku()
+    konteks = {
+        'form': form,
+    }
+    return render(request, 'tambah-buku.html', konteks)
